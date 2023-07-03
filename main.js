6e0a01731663b2fe27656e838613af9c4016582c
Jose Casas <jgpcasas8@gmail.com>
2023-07-02 19:12:28 -0500

    Adding shopping cart, missing javascript

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burguerLogo = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const carIcon = document.querySelector(".shop-icon");
+const carAside = document.querySelector(".product-detail");
+
+function toggleProductDetail() {
+
+    //*carAside.classList.toggle("inactive");
+    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive")
+    const isCarAsideClosed = carAside.classList.contains("inactive")
+
+    if {
+        isDesktopMenuClosed
+    }
+}
+
+carIcon.addEventListener("click", toggleProductDetail);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
