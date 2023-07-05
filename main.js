a4b16ead2662678749f0929fe0b8b2a216ab9c4c
Jose Casas <jgpcasas8@gmail.com>
2023-07-04 19:11:10 -0500

    New commit adding product detail

--- main.js
+++ main.js
@@ -1,31 +1,58 @@
-const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
-const burguerLogo = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-const carIcon = document.querySelector(".shop-icon");
-const carAside = document.querySelector(".product-detail");
+const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
-function toggleProductDetail() {
-
-    //*carAside.classList.toggle("inactive");
-    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive")
-    const isCarAsideClosed = carAside.classList.contains("inactive")
-
-    if {
-        isDesktopMenuClosed
-    }
-}
-
-carIcon.addEventListener("click", toggleProductDetail);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
-}
+  const isAsideClosed = aside.classList.contains('inactive');
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+  
+  desktopMenu.classList.toggle('inactive');
+}
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle("inactive");
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive'); 
+  }
+  
+  mobileMenu.classList.toggle('inactive');
 }
 
-burguerLogo.addEventListener("click", toggleMobileMenu);
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive'); 
+  }
+  
+  aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+  name: "Bike",
+  price: 145.00,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+productList.push({
+  name: "Laptop",
+  price: 559.50,
+  image: "https://www.lenovo.com/medias/lenovo-laptops-thinkbook-16-gen-4-intel-hero.png?context=bWFzdGVyfHJvb3R8MzQ1OTM2fGltYWdlL3BuZ3xoMjEvaGZkLzEzMjU1MTI1OTkxNDU0LnBuZ3xlMGJjMDAyZjIzYzczYmY0YTY3NTlmODcwMDJjZTBhMzg5M2VlMjFlNTNlZWJkZDMyNDA3MTdlNjc3NjhhZWY5"
+})
+productList.push({
+  name: "Celular",
+  price: 340.50,
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ__Qv-z9MZwNNyS_yDrdxu04Nz0K-y88GXGGKS29y30MljhQAMU2lMliWGGgvEUY0gSbY&usqp=CAU"
+})
+
